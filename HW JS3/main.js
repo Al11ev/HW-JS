0d90744220d898dadd4f7cf3fda366804d502fb0
Al11ev <125688134+Al11ev@users.noreply.github.com>
2023-03-24 16:51:45 +0200

    HW

---
+++ HW JS3/main.js
@@ -0,0 +1,87 @@
+// task 1 - String: greeting
+
+// {
+//     let UName = prompt("Write your name here",'');
+//     alert("Hi " + UName + ", WELCOME to the club buddy!");
+// }
+
+// task 2 - String: gopni4ek
+
+// {
+//     let chot = prompt("Write here something with -,-",'');
+//     let choto = chot.split(",").join(`, блін, `);
+//     alert(`${choto}`);
+// }
+
+// task 3 - String: capitalize
+
+// {
+//     let str = "cANBerRa";
+//     let firstLetter = str [0].toUpperCase().slice(0);
+//     let otherLetters = str.slice(1,str.length -0).toLowerCase();
+//     let result = `${firstLetter}${otherLetters}`;
+//     console.log(result);
+// }
+
+// task 4 - String: word count
+
+// {
+//     let str = prompt("Write here your Lorem Ipsum and i can say, how many words in your text",``);
+//     let newStr = str.split(` `).length;
+//     alert(`There are ${newStr} words in your Lorem Ipsum`);
+// }
+
+// task 5 - String: credentials
+
+// {
+//     let surname = prompt("Write your surname",'');
+//     surname = surname.trim();
+//     let name = prompt("Write your name",'');
+//     name = name.trim();
+//     let patronymic = prompt("Write your patronymic",'');
+//     patronymic = patronymic.trim();
+//     let firstSLetter = surname [0].toUpperCase().slice(0);
+//     let firstNLetter = name [0].toUpperCase().slice(0);
+//     let firstPLetter = patronymic [0].toUpperCase().slice(0);
+//     let otherSLetters = surname.slice(1,surname.length -0).toLowerCase();
+//     let otherNLetters = name.slice(1,name.length -0).toLowerCase();
+//     let otherPLetters = patronymic.slice(1,patronymic.length -0).toLowerCase();
+//     let fullName = firstSLetter + otherSLetters + " " + firstNLetter + otherNLetters + " " + firstPLetter + otherPLetters;
+//     console.log(fullName);
+// }
+
+//task 6 - String: beer
+
+// {
+//     let str = "Було жарко. Василь пив пиво вприкуску з креветками";
+//     let result = str.split('пиво').join('чай');
+//     console.log(result);
+// }
+
+//task 7 - String: no tag
+
+// {
+//     let str = "якийсь текст, в якому є один тег <br /> і всяке інше";
+//     "якийсь текст, в якому є один тег <br /> і всяке інше".indexOf("<br />");
+//     let strr = str.slice(0, 33);
+//     let strrr = str.slice(39,53);
+//     let result = strr + strrr;
+//     console.log(result);
+// }
+
+//task 8 - String: big tag
+
+// {
+//     let str = "якийсь текст, в якому є один тег <br /> і всяке інше";
+//     "якийсь текст, в якому є один тег <br /> і всяке інше".indexOf("<br />");
+//     let strr = str.slice(0, 33);
+//     let strrr = str.slice (33, 39).toUpperCase();
+//     let strrrr = str.slice(39,53);
+//     let result = strr + strrr + strrrr;
+//     console.log(result);
+// }
+
+//task 9 - String: new line
+
+//task 10 - String: youtube
+
